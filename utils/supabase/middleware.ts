c08d32ece3cdf8ee3124d626146e6b911e1d1644
diawando <abduldiawade777@gmail.com>
2025-03-04 21:07:31 +0000

    ajout d'update

---
+++ utils/supabase/middleware.ts
@@ -0,0 +1,51 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function updateSession(request: NextRequest){
+      let supabaseResponse = NextResponse.next({
+         request,
+      });
+
+      // creates the Supabase cookie functions
+      const supabase = createServerClient(
+          process.env.NEXT_PUBLIC_SUPABASE_URL!,
+          process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+          {
+            cookies:{
+                getAll(){
+                     return request.cookies.getAll();
+                },
+                setAll(cookiesToSet) {
+                    cookiesToSet.forEach(({ name, value }) =>
+                        request.cookies.set(name, value)
+                    );
+                    supabaseResponse = NextResponse.next({
+                         request,
+                    });
+                    cookiesToSet.forEach(({ name, value, options }) =>
+                       supabaseResponse.cookies.set(name, value, options)
+                    );
+                },
+            },
+          }
+      );
+      // placeholder for protected route controller
+      // gets current user
+      const {
+          data : { user },
+      } = await supabase.auth.getUser();
+      // declares protected routes
+      if(
+        !user && 
+        request.nextUrl.pathname !== "/" &&
+        !request.nextUrl.pathname.startsWith("/instructor/auth") &&
+        !request.nextUrl.pathname.startsWith("/student/auth")
+      ){
+         // Redirect unauthenticade users to the login page
+         const url = request.nextUrl.clone();
+         url.pathname = "/student/auth/login"; // redirect page
+         return NextResponse.redirect(url);
+      }
+      // returns Supabase response
+      return supabaseResponse;
+}
